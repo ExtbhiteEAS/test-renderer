217ffcb013e395972e06e9b754ce26694ed70e3f
ExtbhiteEAS <ExtbhiteEAS@users.noreply.github.com>
2024-06-20 16:12:07 +0000

    deploy: 4f23e35b8cf8af9a788ed28d5c6d2fb5d2db0f33

---
+++ renderer.py
@@ -0,0 +1,4 @@
+manifest = {
+    "source_code": b'ZnJvbSBzcmMgaW1wb3J0IGxvZ3ByaW50CmZyb20gZGlzdC5saWIgaW1wb3J0IHRlc3QKCmRlZiBtYWluX2Z1bmN0aW9uKCk6CiAgICBsb2dwcmludC5kZWJ1ZyhmJ3t0ZXN0LmZ1bmN0aW9uKCl9IHwgVG8gc2VlIGZ1bGwgdHV0b3JpYWwgb2YgdGhhdCwgY2hlY2s6IDxsaW5rPicpCiAgICAKZGVmIHNldHVwKCk6CiAgICByZXR1cm4gbWFpbl9mdW5jdGlvbigp',
+    "extras": [{'path': './dist/lib/test.py', 'code': "b'ZGVmIGZ1bmN0aW9uKCk6CiAgICByZXR1cm4gJ3N0cmluZyB0aGluZyBvbmx5IGdhdmUgdG8gcHJpbnQoKSBmdW5jdGlvbic='"}]
+}
