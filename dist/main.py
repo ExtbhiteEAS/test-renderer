530eff9e10c269896f665df793e8fd380d86cc26
Extbhite <extbhite@ya.ru>
2024-06-23 12:26:48 +0300

    Update main.py

--- dist/main.py
+++ dist/main.py
@@ -1,5 +1,5 @@
 from src import logprint
-from .dist.lib import test
+from dist.lib import test
 
 def main_function():
     logprint.debug(f'{test.function()} | To see full tutorial of that, check: <link>')
