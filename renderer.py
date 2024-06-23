145632840bc5e3b1193c12fa5c62c6324df06a47
ExtbhiteEAS <ExtbhiteEAS@users.noreply.github.com>
2024-06-23 09:15:29 +0000

    deploy: 5282df70427bb4eadd7b1e4155512de09cc8fcc1

--- renderer.py
+++ renderer.py
@@ -1,5 +1,5 @@
 manifest = {
     'info': {'name': 'Your renderer', 'id': 'test_renderer_id', 'author': 'You', 'version': '1.0'},
-    'source_code': b'ZnJvbSBzcmMgaW1wb3J0IGxvZ3ByaW50CmZyb20gZGlzdC5saWIgaW1wb3J0IHRlc3QKCmRlZiBtYWluX2Z1bmN0aW9uKCk6CiAgICBsb2dwcmludC5kZWJ1ZyhmJ3t0ZXN0LmZ1bmN0aW9uKCl9IHwgVG8gc2VlIGZ1bGwgdHV0b3JpYWwgb2YgdGhhdCwgY2hlY2s6IDxsaW5rPicpCiAgICAKZGVmIHNldHVwKCk6CiAgICByZXR1cm4gZXhlYygnbWFpbl9mdW5jdGlvbigpJyk=',
+    'source_code': b'ZnJvbSBzcmMgaW1wb3J0IGxvZ3ByaW50CmZyb20gLmRpc3QubGliIGltcG9ydCB0ZXN0CgpkZWYgbWFpbl9mdW5jdGlvbigpOgogICAgbG9ncHJpbnQuZGVidWcoZid7dGVzdC5mdW5jdGlvbigpfSB8IFRvIHNlZSBmdWxsIHR1dG9yaWFsIG9mIHRoYXQsIGNoZWNrOiA8bGluaz4nKQogICAgCmRlZiBzZXR1cCgpOgogICAgcmV0dXJuIGV4ZWMoJ21haW5fZnVuY3Rpb24oKScp',
     'extras': [{'path': 'dist/lib/test.py', 'code': b'ZGVmIGZ1bmN0aW9uKCk6CiAgICByZXR1cm4gJ3N0cmluZyB0aGluZyBvbmx5IGdhdmUgdG8gcHJpbnQoKSBmdW5jdGlvbic='}]
 }
